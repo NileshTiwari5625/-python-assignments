5b4ec4301c814a9f3720071ab1d75f795ba93236
nilesh <nileshtiwari5625@gmail.com>
2025-06-10 13:33:54 +0530

    first commit

---
+++ Problem-3.py
@@ -0,0 +1,4 @@
+car_license_plate = "MH1234"
+car_weight = 1234.56
+print(type(car_license_plate))
+print(type(car_weight))
---
+++ Problem-4.py
@@ -0,0 +1,6 @@
+bookTitle = "harryPotter"
+BookAuthor = "J.K Rowling"
+publication_year = 1997
+print("bookTitle")
+print("BookAuthor")
+print("publication_year")
